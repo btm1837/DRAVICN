a8b7289668905040c81a7207602fb8e70e87e685
btm1837 <48740743+btm1837@users.noreply.github.com>
2019-04-11 01:26:02 -0400

    Created:  Commented out creation of Trips  fixed .xi to .loc panda references  added import numpy as np  added get_Var()
    Signed-off-by: btm1837 <48740743+btm1837@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pyomo
 import pandas
+import numpy as np
 import pyomo.opt
 import pyomo.environ as pe
 
@@ -31,14 +32,19 @@ class MinCostFlow:
         self.arc_data.set_index(['Start', 'End'], inplace=True)
         self.arc_data.sort_index(inplace=True)
 
+        #self.trip_data = pandas.read_csv('trips.csv')
+        #self.trip_data.set_index(['Origin','Dest'], inplace=True)
+        #self.trip_data.sort_index(inplace=True)
+
         self.node_set = self.node_data.index.unique()
         self.arc_set = self.arc_data.index.unique()
+        #self.trip_set = self.trip_data.index.unique()
 
         self.createModel()
 
     def createModel(self):
         """Create the pyomo model given the csv data."""
-        self.m = pe.ConcreteModel()
+        self.m = pe.AbstractModel()
 
         # Create sets
         self.m.node_set = pe.Set(initialize=self.node_set)
@@ -49,16 +55,16 @@ class MinCostFlow:
 
         # Create objective
         def obj_rule(m):
-            return sum(m.Y[e] * self.arc_data.ix[e, 'Cost'] for e in self.arc_set)
+            return sum(m.Y[e] * self.arc_data.loc[e, 'Cost'] for e in self.arc_set)
 
         self.m.OBJ = pe.Objective(rule=obj_rule, sense=pe.minimize)
 
-        # Flow Ballance rule
+        # Flow Balance rule
         def flow_bal_rule(m, n):
             arcs = self.arc_data.reset_index()
             preds = arcs[arcs.End == n]['Start']
             succs = arcs[arcs.Start == n]['End']
-            return sum(m.Y[(p, n)] for p in preds) - sum(m.Y[(n, s)] for s in succs) == self.node_data.ix[
+            return sum(m.Y[(p, n)] for p in preds) - sum(m.Y[(n, s)] for s in succs) == self.node_data.loc[
                 n, 'Imbalance']
 
         self.m.FlowBal = pe.Constraint(self.m.node_set, rule=flow_bal_rule)
@@ -66,16 +72,16 @@ class MinCostFlow:
         # Upper bounds rule
         def upper_bounds_rule(m, n1, n2):
             e = (n1, n2)
-            if self.arc_data.ix[e, 'UpperBound'] < 0:
+            if self.arc_data.loc[e, 'UpperBound'] < 0:
                 return pe.Constraint.Skip
-            return m.Y[e] <= self.arc_data.ix[e, 'UpperBound']
+            return m.Y[e] <= self.arc_data.loc[e, 'UpperBound']
 
         self.m.UpperBound = pe.Constraint(self.m.arc_set, rule=upper_bounds_rule)
 
         # Lower bounds rule
         def lower_bounds_rule(m, n1, n2):
             e = (n1, n2)
-            if self.arc_data.ix[e, 'LowerBound'] < 0:
+            if self.arc_data.loc[e, 'LowerBound'] < 0:
                 return pe.Constraint.Skip
             return m.Y[e] >= self.arc_data.ix[e, 'LowerBound']
 
@@ -83,18 +89,25 @@ class MinCostFlow:
 
     def solve(self):
         """Solve the model."""
+        self.i = self.m.create_instance()
         solver = pyomo.opt.SolverFactory('gurobi')
-        results = solver.solve(self.m, tee=True, keepfiles=False,
+        results = solver.solve(self.i, tee=True, keepfiles=False,
                                options_string="mip_tolerances_integrality=1e-9 mip_tolerances_mipgap=0")
 
         if (results.solver.status != pyomo.opt.SolverStatus.ok):
             logging.warning('Check solver not ok?')
         if (results.solver.termination_condition != pyomo.opt.TerminationCondition.optimal):
             logging.warning('Check solver optimality?')
+    def get_Var(self):
+        """Get the variable value and put into dictionary Y"""
+        Y={}
+        for var in sp.i.Y:
+            Y[var]=sp.i.Y[var].value
+        return Y
 
 
 if __name__ == '__main__':
     sp = MinCostFlow('nodes.csv', 'arcs.csv')
     sp.solve()
     print('\n\n---------------------------')
-    print('Cost: ', sp.m.OBJ())
+    print('Cost: ', sp.i.OBJ())
