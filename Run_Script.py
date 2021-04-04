aded65ae3bd78b16bb0d71c7aa513097ae07dbf0
Ben M <benmarus88@gmail.com>
2021-04-04 14:32:13 -0400

    altered run_script

--- Run_Script.py
+++ Run_Script.py
@@ -11,18 +11,20 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
-start_time = time.time()
+
 
 
 # Initializing the model
-def initialize_setup():
-    run_id = 'setup_1'
-    path = r'D:\Documents\Thesis_Docs\experiment_files'
+def initialize_setup(run_id,path,arc_file,trip_file,experiment_file,vehicle_file):
+    # run_id = 'setup_1'
+    # base_path = r'D:\Documents\Thesis_Docs\experiment_files'
+    # arc_file = 'arcs_'+ run_id+'.csv'
+    # trip_file = 'source_sink_' + run_id + '.csv'
+    # experiment_file = r"Experiments.csv"
+    # vehicle_file = r"vehicles.csv"
+
     path = os.path.join(path,run_id)
-    arc_file = 'arcs_'+ run_id+'.csv'
-    trip_file = 'source_sink_' + run_id + '.csv'
-    experiment_file = r"Experiments.csv"
-    vehicle_file = r"vehicles.csv"
+
 
     # arc_file = 'arcs.csv'
     # arc_file = r'arcs_set3.csv'
@@ -34,7 +36,6 @@ def initialize_setup():
     # cell_iteration_dict_file = r"cell_iteration_list.csv"
 
     simulation_time = 0
-
     data = Data_Generator.simulation(arc_file=arc_file,
                                      trip_file=trip_file,
                                      vehicle_file=vehicle_file,
@@ -78,9 +79,8 @@ def transaction_manager_sim_loop(simulation_time,data):
             simulation_time=simulation_time)
 
     # TM adjustments after CTM & ICP before Opt :
-    #   -
     data.transaction_manager_post_vehicle_move(simulation_time=simulation_time)
-    # getting new routing from opt
+    # getting new routing from Opt :
     optimization_model = Optimization.MinCostFlow(node_set=data.get_node_set(),
                                               trip_set=data.get_trip_set(),
                                               trip_net_demand=data.get_trip_net_demand(),
@@ -88,12 +88,18 @@ def transaction_manager_sim_loop(simulation_time,data):
                                               arc_cost=data.get_arc_cost(),
                                               arc_set=data.get_arc_set())
     # solve model
-    optimization_model.solve()
     print('\n\n---------------------------')
+    opt_time_start = time.time()
+    print(f'optimization start time: {opt_time_start}')
+    optimization_model.solve()
+    opt_time_end = time.time()
+    print(f'optimization end time: {opt_time_end}')
+    print(f'optimization solve time: {opt_time_start-opt_time_end}')
+
     print('Cost: ', optimization_model.i.OBJ())
     optimization_model.get_Var()
 
-    #record some data
+    # record some data
     opt_list = [simulation_time,optimization_model.i.OBJ()]
     temp = pd.DataFrame([opt_list],columns=data.columns_opt)
     data.df_opt = data.df_opt.append(temp)
@@ -105,23 +111,39 @@ def transaction_manager_sim_loop(simulation_time,data):
 
     return
 
-def run_simulation():
-    data = initialize_setup()
+def run_simulation(run_id,path,arc_file,trip_file,experiment_file,vehicle_file):
+    data = initialize_setup(run_id,path,arc_file,trip_file,experiment_file,vehicle_file)
     stop_list = [(i * 10 * data.exper_simulation_time_interval) for i in range(10)]
     for simulation_time in range(int(30),int(data.exper_total_sim_time),int(data.exper_simulation_time_interval)):
         transaction_manager_sim_loop(simulation_time=simulation_time,
                                      data= data)
-        print(simulation_time)
+        print(f"simulation_time is :{simulation_time}")
 
         if simulation_time in stop_list:
             print('sim time: \t' + str(simulation_time))
+            print(f'Number of vehicles: ')
 
+    # path = r'D:\Documents\Thesis_Docs\experiment_files'
+    data.df_vehicles.to_csv(os.path.join(path,'vehicle_OUTPUT.txt'),sep='\t')
+    data.df_opt.to_csv(os.path.join(path, 'opt_OUTPUT.txt'), sep='\t')
     return data.df_vehicles,data.df_opt
 
 
 # data = initialize_setup()
 # run_simulation()
-df_vehicles,df_opt = run_simulation()
+
+
+run_id = 'setup_2'
+path = r'D:\Documents\Thesis_Docs\experiment_files'
+# path = os.path.join(path, run_id)
+arc_file = 'arcs_' + run_id + '.csv'
+trip_file = 'source_sink_' + run_id + '.csv'
+experiment_file = r"Experiments.csv"
+vehicle_file = r"vehicles.csv"
+
+
+start_time = time.time()
+df_vehicles,df_opt = run_simulation(run_id,path,arc_file,trip_file,experiment_file,vehicle_file)
 end_time = time.time()
 print("--- runtime = %s seconds ---" %(end_time - start_time))
 
